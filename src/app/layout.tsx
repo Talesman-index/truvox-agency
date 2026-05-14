907f9a451de23f6b5dd161dc551014a0aa1356f5
Talesman-index <dafiashalom@gmail.com>
2026-05-14 18:48:44 +0100

    feat: install Google Tag Manager (GTM-PC3HNC9G)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Archivo } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
 
 const archivo = Archivo({
@@ -50,7 +51,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={archivo.variable}>
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PC3HNC9G');`,
+          }}
+        />
+      </head>
       <body className={`${archivo.className} antialiased min-h-screen flex flex-col overflow-x-hidden`}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PC3HNC9G"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          >
+          </iframe>
+        </noscript>
         <Navbar />
         <main className="flex-grow">
           {children}
